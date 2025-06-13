9624a08396d1af884ea266e64e1b4d8ae811f8c0
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2025-06-13 16:39:33 +0200

    fix(typeColor): now using the array of typecolor instead of object.

--- src/components/type/index.jsx
+++ src/components/type/index.jsx
@@ -5,10 +5,15 @@ import { stringNormalize } from "@services/normalize";
 // Show the name of the pokemon type and add color
 export function Types({pokemonTypes}){
 
+    const getTypeColor = (typeName) => {
+        const objectColor = TYPES.find(type => stringNormalize(type.name) === stringNormalize(typeName))?.color
+        return objectColor
+
+    }
     return(
         <ul className={s.container}>
             {pokemonTypes.map(type => (
-                <li key={type} style={{backgroundColor: TYPES[stringNormalize(type)]}}>{type}</li>
+                <li key={type} style={{backgroundColor: getTypeColor(type)}}>{type}</li>
             ))}
         </ul>
     )
