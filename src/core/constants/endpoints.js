a97b822be8198c46dfe9c5d09182a132feb59f23
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2025-06-13 16:11:37 +0200

    feat(pokemonList): use the pokemonCard component for show picture of the pokemon and id and name

--- src/core/constants/endpoints.js
+++ src/core/constants/endpoints.js
@@ -1,7 +1,10 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
-console.log(BACKEND_URL)
+
+
+
 export const ENDPOINTS = Object.freeze({
-    GET_POKEMONS:`${BACKEND_URL}/pokemons`
+    GET_POKEMONS:`${BACKEND_URL}/pokemons`,
+    GET_IMAGE:(pokemonID) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg`
 })
 
 
--- src/pages/home/components/pokemonCard/index.jsx
+++ src/pages/home/components/pokemonCard/index.jsx
@@ -1,11 +1,16 @@
+import { callBackend } from "@services/backend";
 import s from "./style.module.scss";
+import { ENDPOINTS } from "@constants/endpoints";
 
 export function PokemonCard({pokemon}){
 
-
     return(
-        <>
-            Default
-        </>
+        <li className={s.container}>
+            <div className={s.image_container}>
+                <img src={ENDPOINTS.GET_IMAGE(pokemon.id)} alt={`image of ${pokemon.name}`} />
+            </div>
+            <span className={s.id_name}>#{pokemon.id} {pokemon.name}</span>
+
+        </li>
     )
 }
