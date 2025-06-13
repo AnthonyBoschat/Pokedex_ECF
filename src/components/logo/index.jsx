fe1029624509d0cf8757f990eeea438df55484d6
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2025-06-13 17:36:20 +0200

    fix(logo): now click on the logo of the header redirect at the homePage

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,6 +1,7 @@
 import s from "./style.module.scss";
 import pokeball from "@assets/pokeball.png";
 import pokedex from "@assets/pokedex.png";
+import { ROUTES } from "@constants/routes";
 import { Link } from "react-router";
 
 // The logo of the application
@@ -8,7 +9,7 @@ export function Logo(){
 
 
     return(
-        <Link className={s.container}>
+        <Link to={ROUTES.HOME} className={s.container}>
             <img className={s.pokeball} src={pokeball} alt="" />
             <img className={s.pokedex} src={pokedex} alt="" />
         </Link>
